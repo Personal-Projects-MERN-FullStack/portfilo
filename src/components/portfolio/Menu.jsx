3bb58a04607a36a910951d9f9a6ec03ad160f450
vaibhav-vm-vidhya <vaibhavvakil1234@gmail.com>
2023-06-15 11:53:38 +0530

    iamge projecobt repoove

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -1,11 +1,9 @@
-import work1 from "../../assets/Work.png";
-import work2 from "../../assets/Work1.png";
-import work3 from "../../assets/Work2.png";
+
 //image should be 330pxX267px
 const Menu = [
   {
     id: 1,
-    image: work1,
+    image: "https://dl.dropboxusercontent.com/s/2x361pebgqt1vv3/Work.png",
     title: "Labour Management System",
     category: "Web Application",
     url: "https://labourconnect.online",
@@ -14,16 +12,17 @@ const Menu = [
   },
   {
     id: 2,
-    image: work2,
+    image: 'https://dl.dropboxusercontent.com/s/zdvi716bzdmwwrm/work1.png',
     title: "Hospital Managment",
     category: "Web Development",
     url: "http://bloomwithus.co.in/",
     repositoryUrl:
       "https://github.com/Php-Projects-Internship-Rudrakasha/Medicio.git",
   },
+  // https://www.dropbox.com/s/s3dij7pbl2i6s81/Work2.png?dl=0
   {
     id: 3,
-    image: work3,
+    image: "https://dl.dropboxusercontent.com/s/s3dij7pbl2i6s81/Work2.png",
     title: "Gold Shop Managment",
     category: "Web Development",
     url: "https://aksheyjewellers.com/",
